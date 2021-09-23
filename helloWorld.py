ebe283ef3766d1e29b6d7adbb124f88c9da2bced
vtuberion <72401317+vtuberion@users.noreply.github.com>
2021-09-23 14:20:24 -0400

    Delete helloWorld.py

--- helloWorld.py
+++
@@ -1,6 +0,0 @@
-# helloWorld.py
-# Made by Vincent Tuberion
-# Date Created: 9/22/2021 12:49 EST
-# Date Modified: 9/22/2021 12:50 EST
-# This is Version 1 of my hello world program
-print("Hello my name is Vincent Tuberion")
